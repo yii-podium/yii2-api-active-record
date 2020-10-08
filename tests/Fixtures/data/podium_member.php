6139a3db8ebfaa12a40fb7a63fa908354425ff92
Bizley <pawel@positive.codes>
2020-10-08 13:50:26 +0200

    Member fixtures

--- tests/Fixtures/data/podium_member.php
+++ tests/Fixtures/data/podium_member.php
@@ -1,1 +1,24 @@
 <?php
+
+use Podium\ActiveRecordApi\Enums\MemberStatus;
+
+return [
+    'member1' => [
+        'id' => 1,
+        'user_id' => '1',
+        'username' => 'Member1',
+        'slug' => 'Member1',
+        'status_id' => MemberStatus::ACTIVE,
+        'created_at' => 1,
+        'updated_at' => 1,
+    ],
+    'member2' => [
+        'id' => 2,
+        'user_id' => '2',
+        'username' => 'Member2',
+        'slug' => 'Member2',
+        'status_id' => MemberStatus::BANNED,
+        'created_at' => 1,
+        'updated_at' => 1,
+    ],
+];
