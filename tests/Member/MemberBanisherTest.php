9f10c9c0ae8e7c5955d6f28986ba31bcb9fc2a27
Bizley <pawel@positive.codes>
2020-10-08 13:50:59 +0200

    Member Banisher tests

---
+++ tests/Member/MemberBanisherTest.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Podium\Tests\Member;
+
+use Podium\ActiveRecordApi\ActiveRecords\MemberActiveRecord;
+use Podium\ActiveRecordApi\Enums\MemberStatus;
+use Podium\ActiveRecordApi\Repositories\MemberRepository;
+use Podium\Tests\DbTestCase;
+use Podium\Tests\Fixtures\MemberFixture;
+
+class MemberBanisherTest extends DbTestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+        (new MemberFixture())->load();
+    }
+
+    protected function tearDown(): void
+    {
+        (new MemberFixture())->unload();
+    }
+
+    public function testBanning(): void
+    {
+        $member = MemberActiveRecord::findOne(1);
+        self::assertSame(MemberStatus::ACTIVE, $member->status_id);
+
+        $repository = new MemberRepository();
+        $repository->setModel($member);
+
+        $response = $this->podium->member->ban($repository);
+        self::assertTrue($response->getResult());
+        self::assertTrue($repository->isBanned());
+
+        $member = MemberActiveRecord::findOne(1);
+        self::assertSame(MemberStatus::BANNED, $member->status_id);
+    }
+
+    public function testUnbanning(): void
+    {
+        $member = MemberActiveRecord::findOne(2);
+        self::assertSame(MemberStatus::BANNED, $member->status_id);
+
+        $repository = new MemberRepository();
+        $repository->setModel($member);
+
+        $response = $this->podium->member->unban($repository);
+        self::assertTrue($response->getResult());
+        self::assertFalse($repository->isBanned());
+
+        $member = MemberActiveRecord::findOne(2);
+        self::assertSame(MemberStatus::ACTIVE, $member->status_id);
+    }
+
+    public function testFailedBanning(): void
+    {
+        $member = MemberActiveRecord::findOne(2);
+        self::assertSame(MemberStatus::BANNED, $member->status_id);
+
+        $repository = new MemberRepository();
+        $repository->setModel($member);
+
+        $response = $this->podium->member->ban($repository);
+        self::assertFalse($response->getResult());
+        self::assertSame(['api' => 'Member is already banned.'], $response->getErrors());
+    }
+
+    public function testFailedUnbanning(): void
+    {
+        $member = MemberActiveRecord::findOne(1);
+        self::assertSame(MemberStatus::ACTIVE, $member->status_id);
+
+        $repository = new MemberRepository();
+        $repository->setModel($member);
+
+        $response = $this->podium->member->unban($repository);
+        self::assertFalse($response->getResult());
+        self::assertSame(['api' => 'Member has not been banned.'], $response->getErrors());
+    }
+}
