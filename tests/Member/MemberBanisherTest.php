687b098dcb4f17ff9589c7998cb1a77a14a8cfee
Bizley <pawel@positive.codes>
2020-10-11 13:23:07 +0200

    Member removing tests

--- tests/Member/MemberBanisherTest.php
+++ tests/Member/MemberBanisherTest.php
@@ -52,30 +52,4 @@ class MemberBanisherTest extends DbTestCase
         self::assertSame(MemberStatus::ACTIVE, $member->status_id);
         self::assertEqualsWithDelta(time(), $member->updated_at, 10);
     }
-
-    public function testFailedBanning(): void
-    {
-        $member = MemberActiveRecord::findOne(2);
-        self::assertSame(MemberStatus::BANNED, $member->status_id);
-
-        $repository = new MemberRepository();
-        $repository->setModel($member);
-
-        $response = $this->podium->member->ban($repository);
-        self::assertFalse($response->getResult());
-        self::assertSame(['api' => 'Member is already banned.'], $response->getErrors());
-    }
-
-    public function testFailedUnbanning(): void
-    {
-        $member = MemberActiveRecord::findOne(1);
-        self::assertSame(MemberStatus::ACTIVE, $member->status_id);
-
-        $repository = new MemberRepository();
-        $repository->setModel($member);
-
-        $response = $this->podium->member->unban($repository);
-        self::assertFalse($response->getResult());
-        self::assertSame(['api' => 'Member has not been banned.'], $response->getErrors());
-    }
 }
