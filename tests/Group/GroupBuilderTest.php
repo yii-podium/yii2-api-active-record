10b3cdf03a59072204676569bf4f1610b6ebeb8f
Bizley <pawel@positive.codes>
2021-03-27 12:03:28 +0100

    Tests

--- tests/Group/GroupBuilderTest.php
+++ tests/Group/GroupBuilderTest.php
@@ -9,6 +9,8 @@ use Podium\ActiveRecordApi\Repositories\GroupRepository;
 use Podium\Tests\DbTestCase;
 use Podium\Tests\Fixtures\GroupFixture;
 
+use function str_repeat;
+
 class GroupBuilderTest extends DbTestCase
 {
     public function fixtures(): array
@@ -40,5 +42,29 @@ class GroupBuilderTest extends DbTestCase
         self::assertSame('Group Edited', $group->name);
         self::assertNotEqualsWithDelta(time(), $group->created_at, 10);
         self::assertEqualsWithDelta(time(), $group->updated_at, 10);
+    }
+
+    public function testCreatingWithoutData(): void
+    {
+        $response = $this->podium->group->create();
+
+        self::assertFalse($response->getResult());
+        self::assertSame([], $response->getErrors());
+    }
+
+    public function testCreatingWithTooLongName(): void
+    {
+        $response = $this->podium->group->create(['name' => str_repeat('a', 192)]);
+
+        self::assertFalse($response->getResult());
+        self::assertSame(['name' => ['Group Name should contain at most 191 characters.']], $response->getErrors());
+    }
+
+    public function testCreatingWithTakenName(): void
+    {
+        $response = $this->podium->group->create(['name' => 'Group 1']);
+
+        self::assertFalse($response->getResult());
+        self::assertSame(['name' => ['Group Name "Group 1" has already been taken.']], $response->getErrors());
     }
 }
